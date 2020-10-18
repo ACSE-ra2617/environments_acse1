4e9f5c46cf74d6fba685f423911c9a32ec60c0d6
rauladriaensen98 <35867802+rauladriaensen98@users.noreply.github.com>
2020-10-18 21:04:02 +0100

    generate image file

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import cv2
 
 __all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
 
@@ -8,8 +9,19 @@ __all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
 def rand_array(shape):
     return np.random.rand(*shape)
 
+
 def smooth_image(a, sigma=1):
     return gaussian_filter(a, sigma=sigma)
 
+
 def my_mat_solve(A, b):
     return A.inv()*b
+
+
+def create_2_colour_image(height, width):
+    blank_image = np.zeros((height, width, 3), np.uint8)
+    blank_image[:, 0:width//2] = (255, 0, 0)  # (B, G, R)
+    blank_image[:, width//2:width] = (0, 255, 0)
+    cv2.imshow('image', blank_image)
+    cv2.waitKey(0)
+    return blank_image
---
+++ scripts/random_image.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 18 20:58:16 2020
+
+@author: raula
+"""
+
+from envtest import create_2_colour_image
+
+width = 600
+height = 200
+create_2_colour_image(height, width)
